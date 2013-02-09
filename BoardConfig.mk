bdd788d32c1c6a1ef8c00fe1ca70a3de66f3001a
singh_dd93 <singh_dd93@yahoo.in>
2013-02-10 01:42:44 +0530

    Build with linaro and fixed dependencies
    Change-Id: I1726134d33e50e05e462a66ce6aa5b5cf0e6b7f3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,8 +2,8 @@
 -include vendor/semc/mango/BoardConfigVendor.mk
 
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
-TARGET_KERNEL_CONFIG := cyanogen_mango_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
+TARGET_KERNEL_CONFIG := pa_mango_defconfig
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-gnueabihf-linaro-4.7
 
 TARGET_RECOVERY_INITRC := device/semc/mango/recovery/init.rc
 
